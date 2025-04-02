f4b2bd8ee6800bbd68b11abe6446b77d77b81930
Shafayet <11shafayet@gmail.com>
2025-04-02 12:08:41 +0600

    updated portfolio code

---
+++ src/data/staticPortfolioData.js
@@ -0,0 +1,237 @@
+import p1banner from '/public/images/project1/banner.png';
+import p2banner from '/public/images/project2/banner.png';
+import p3banner from '/public/images/project3/banner.png';
+import p4banner from '/public/images/project4/banner.png';
+import p5banner from '/public/images/project5/banner.png';
+import p6banner from '/public/images/project6/banner.png';
+
+import p1main from '/public/images/project1/main.png';
+import p2main from '/public/images/project2/main.png';
+import p3main from '/public/images/project3/main.png';
+import p4main from '/public/images/project4/main.png';
+import p5main from '/public/images/project5/main.png';
+import p6main from '/public/images/project6/main.png';
+
+import p1show from '/public/images/project1/show.png';
+import p2show from '/public/images/project2/show.png';
+import p3show from '/public/images/project3/show.png';
+import p4show from '/public/images/project4/show.png';
+import p5show from '/public/images/project5/show.png';
+import p6show from '/public/images/project6/show.png';
+
+import p1top1 from '/public/images/project1/top1.png';
+import p1top2 from '/public/images/project1/top2.png';
+import p1top3 from '/public/images/project1/top3.png';
+
+import p2top1 from '/public/images/project2/top1.png';
+import p2top2 from '/public/images/project2/top2.png';
+import p2top3 from '/public/images/project2/top3.png';
+
+import p3top1 from '/public/images/project3/top1.png';
+import p3top2 from '/public/images/project3/top2.png';
+import p3top3 from '/public/images/project3/top3.png';
+
+import p4top1 from '/public/images/project4/top1.png';
+import p4top2 from '/public/images/project4/top2.png';
+import p4top3 from '/public/images/project4/top3.png';
+
+import p5top1 from '/public/images/project5/top1.png';
+import p5top2 from '/public/images/project5/top2.png';
+import p5top3 from '/public/images/project5/top3.png';
+
+import p6top1 from '/public/images/project6/top1.png';
+import p6top2 from '/public/images/project6/top2.png';
+import p6top3 from '/public/images/project6/top3.png';
+
+import p1bottom1 from '/public/images/project1/bottom1.png';
+import p1bottom2 from '/public/images/project1/bottom2.png';
+import p1bottom3 from '/public/images/project1/bottom3.png';
+import p1bottom4 from '/public/images/project1/bottom4.png';
+import p1bottom5 from '/public/images/project1/bottom5.png';
+import p1bottom6 from '/public/images/project1/bottom6.png';
+import p1bottom7 from '/public/images/project1/bottom7.png';
+
+import p2bottom1 from '/public/images/project2/bottom1.png';
+import p2bottom2 from '/public/images/project2/bottom2.png';
+import p2bottom3 from '/public/images/project2/bottom3.png';
+import p2bottom4 from '/public/images/project2/bottom4.png';
+import p2bottom5 from '/public/images/project2/bottom5.png';
+import p2bottom6 from '/public/images/project2/bottom6.png';
+import p2bottom7 from '/public/images/project2/bottom7.png';
+
+import p3bottom1 from '/public/images/project3/bottom1.png';
+import p3bottom2 from '/public/images/project3/bottom2.png';
+import p3bottom3 from '/public/images/project3/bottom3.png';
+import p3bottom4 from '/public/images/project3/bottom4.png';
+import p3bottom5 from '/public/images/project3/bottom5.png';
+import p3bottom6 from '/public/images/project3/bottom6.png';
+import p3bottom7 from '/public/images/project3/bottom7.png';
+
+import p4bottom1 from '/public/images/project4/bottom1.png';
+import p4bottom2 from '/public/images/project4/bottom2.png';
+import p4bottom3 from '/public/images/project4/bottom3.png';
+import p4bottom4 from '/public/images/project4/bottom4.png';
+import p4bottom5 from '/public/images/project4/bottom5.png';
+import p4bottom6 from '/public/images/project4/bottom6.png';
+import p4bottom7 from '/public/images/project4/bottom7.png';
+
+import p5bottom1 from '/public/images/project5/bottom1.png';
+import p5bottom2 from '/public/images/project5/bottom2.png';
+import p5bottom3 from '/public/images/project5/bottom3.png';
+import p5bottom4 from '/public/images/project5/bottom4.png';
+import p5bottom5 from '/public/images/project5/bottom5.png';
+import p5bottom6 from '/public/images/project5/bottom6.png';
+import p5bottom7 from '/public/images/project5/bottom7.png';
+
+import p6bottom1 from '/public/images/project6/bottom1.png';
+import p6bottom2 from '/public/images/project6/bottom2.png';
+import p6bottom3 from '/public/images/project6/bottom3.png';
+import p6bottom4 from '/public/images/project6/bottom4.png';
+import p6bottom5 from '/public/images/project6/bottom5.png';
+import p6bottom6 from '/public/images/project6/bottom6.png';
+import p6bottom7 from '/public/images/project6/bottom7.png';
+
+export const portfolioData = [
+  {
+    id: 1,
+    title: 'Atelier Forma | Interior Studio',
+    study: `Atelier Forma | Interior Studio`,
+    platform: `Wordpress`,
+    vibe: `Elegant, Craftsmanship, Timeless , Aesthetics,  Harmonious `,
+    desc: `A website & brand design for a interior design studio dedicated to curating timeless spaces that embody elegance, craftsmanship, and modern sensibility. With a refined approach that balances heritage and innovation, Atelier Forma transforms interiors into immersive, harmonious experiences.Atelier Forma required a brand identity that seamlessly reflects its philosophy—merging architectural tradition with contemporary refinement. The goal was to create a cohesive visual language that conveys its dedication to curated aesthetics, material integrity, and thoughtful spatial design. `,
+    categories: [`Website in a day`],
+    showImage: p1show,
+    mainImage: p1main,
+    bannerImage: p1banner,
+    topImages: [p1top1, p1top2, p1top3],
+    bottomImages: [
+      p1bottom1,
+      p1bottom2,
+      p1bottom3,
+      p1bottom4,
+      p1bottom5,
+      p1bottom6,
+      p1bottom7,
+    ],
+  },
+  {
+    id: 2,
+    title: 'SERENE & SPACE Interior Studio ',
+    study: `SERENE and SPACE | Interior Design Studio`,
+    platform: `Wordpress`,
+    vibe: `Elegant, Minimal , Elevated,Timeless`,
+    desc: `From tranquil homes to sophisticated commercial interiors, they craft designs that balance beauty and purpose, reflecting their clients’ unique visions.
+The logo features a refined serif font with delicate strokes, embodying elegance and clarity. Supporting branding elements, including a versatile submark, ensure consistency across all touchpoints.
+
+This minimalist and cohesive identity captures SERENE and SPACE’s ethos—where elegance meets functionality, and every space tells a story.`,
+    categories: [`Branding`],
+    showImage: p2show,
+    mainImage: p2main,
+    bannerImage: p2banner,
+    topImages: [p2top1, p2top2, p2top3],
+    bottomImages: [
+      p2bottom1,
+      p2bottom2,
+      p2bottom3,
+      p2bottom4,
+      p2bottom5,
+      p2bottom6,
+      p2bottom7,
+    ],
+  },
+  {
+    id: 3,
+    title: 'Bloom Bliss | Wedding Planner',
+    study: `Bloom Bliss | Wedding Planner`,
+    platform: `Wordpress`,
+    vibe: `Elegant, Classic, romantic, elevated,  timeless`,
+    desc: `Capture Emotion: A logo suite that radiates elegance and evokes joy at first glance.Unified Visuals: Seamless design elements that align with your services, from floral arrangements to flawless ceremonies.
+Timeless Identity: A brand that feels luxurious yet approachable, leaving a lasting impression on your clients.`,
+    categories: [`Branding`],
+    showImage: p3show,
+    mainImage: p3main,
+    bannerImage: p3banner,
+    topImages: [p3top1, p3top2, p3top3],
+    bottomImages: [
+      p3bottom1,
+      p3bottom2,
+      p3bottom3,
+      p3bottom4,
+      p3bottom5,
+      p3bottom6,
+      p3bottom7,
+    ],
+  },
+  {
+    id: 4,
+    title: 'Shapeshape // Architecture Agency',
+    study: `Shapeshape // Architecture Agency`,
+    platform: `MERN | Custom Coding`,
+    vibe: `Elegant, Bold, Minimal, Harmony,  Warm`,
+    desc: `Life Among Nature – A seamless blend of modern architecture and natural serenity.
+
+The goal was to create a visually immersive experience that highlights the beauty of sustainable, nature-inspired living. Key features:  Minimalistic layout for an elegant user journey. Bold visuals to emphasise architectural brilliance. Warm tones and textures that reflect harmony with nature.`,
+    categories: [`Custom Website`],
+    showImage: p4show,
+    mainImage: p4main,
+    bannerImage: p4banner,
+    topImages: [p4top1, p4top2, p4top3],
+    bottomImages: [
+      p4bottom1,
+      p4bottom2,
+      p4bottom3,
+      p4bottom4,
+      p4bottom5,
+      p4bottom6,
+      p4bottom7,
+    ],
+  },
+  {
+    id: 5,
+    title: 'Illuminate |  Website design',
+    study: `Illuminate |  Website design`,
+    platform: `MERN | Custom Coding`,
+    vibe: `Elegant, Minimal, Elevated, Timeless`,
+    desc: `A website  for a lamp-light  online store. A passion project here at Design Arclight aimed at creating a timeless, sophisticated online presence for a lamp store.
+
+Let’s Create the perfect glow for your home. Your home Is an extension of your soul & the light in it should reflect who you are.`,
+    categories: [`Custom Website`],
+    showImage: p5show,
+    mainImage: p5main,
+    bannerImage: p5banner,
+    topImages: [p5top1, p5top2, p5top3],
+    bottomImages: [
+      p5bottom1,
+      p5bottom2,
+      p5bottom3,
+      p5bottom4,
+      p5bottom5,
+      p5bottom6,
+      p5bottom7,
+    ],
+  },
+  {
+    id: 6,
+    title: 'Redwood Rise | Leadership  Coach',
+    study: `Redwood Rise | Leadership  Coach`,
+    platform: `Wordpress`,
+    vibe: `Endurance , Strength, Grow,  Deep-rooted,   Rising`,
+    desc: `Rooted in Strength, Rising in Impact. For Redwood Rise, I wanted to create a brand identity that reflects resilience, transformation, and deep-rooted leadership.
+
+Inspired by towering redwoods—symbols of endurance and strength—this brand embodies the belief that great leadership is built over time, weathering challenges and growing stronger with each experience.`,
+    categories: [`Branding`],
+    showImage: p6show,
+    mainImage: p6main,
+    bannerImage: p6banner,
+    topImages: [p6top1, p6top2, p6top3],
+    bottomImages: [
+      p6bottom1,
+      p6bottom2,
+      p6bottom3,
+      p6bottom4,
+      p6bottom5,
+      p6bottom6,
+      p6bottom7,
+    ],
+  },
+];
--- src/pages/PortfolioDetails.jsx
+++ src/pages/PortfolioDetails.jsx
@@ -1,222 +1,182 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { toast } from 'react-toastify';
 import Marquee from 'react-fast-marquee';
-import { useQuery } from '@tanstack/react-query';
 import SecButton from '../components/common/SecButton';
+import { portfolioData } from '../data/staticPortfolioData';
 
 const texts = [
   `Brand design . web design.  website in a day .Brand design . web design.  website in a day . Brand design . web design.  website in a day .  Brand design . web design.  website in a day .  Brand design . web design.  website in a day .`,
   `Brand design . web design.  website in a day .Brand design . web design.  website in a day . Brand design . web design.  website in a day .  Brand design . web design.  website in a day .  Brand design . web design.  website in a day . `,
 ];
 const PortfolioDetails = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
   const [projectData, setProjectData] = useState({});
   const params = useParams();
   const projectId = params.id;
-  const imgUrl =
-    'https://res.cloudinary.com/deqpfnzrp/image/upload/v1740149985';
-
-  // Fetch project data
-  const {
-    data: project,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ['project'],
-    queryFn: async () => {
-      const response = await fetch(
-        `${import.meta.env.VITE_SERVER_URL}/api/projects/${projectId}`
-      );
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      return data.data || [];
-    },
-    onSuccess: (data) => {
-      setProjectData(data);
-    },
-  });
 
   useEffect(() => {
-    if (project) {
-      setProjectData(project);
-      console.log(project);
-    }
-  }, [project]);
+    window.scrollTo(0, 0);
+    const project = portfolioData.find((item) => item.id === Number(projectId));
+    setProjectData(project || {});
+    console.log(project);
+  }, [projectId]);
 
-  if (error) {
-    toast.error(error.message);
-    return <div>Error: {error.message}</div>;
-  }
   return (
     <>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          {/* banner image */}
-          <div>
+      <>
+        {/* banner image */}
+        <div>
+          <img src={projectData?.bannerImage} alt="banner Image" />
+        </div>
+
+        {/* marquee */}
+        <div className="w-full border-y border-[#577C8E] text-theme py-4">
+          <Marquee speed={50}>
+            {texts.map((item, i) => (
+              <h4 key={i} className="text-lg capitalize">
+                {item}
+              </h4>
+            ))}
+          </Marquee>
+        </div>
+
+        <div className="container">
+          {/* content */}
+          <div className="flex flex-col-reverse md:flex-row justify-between md:items-center gap-4 my-12">
+            <div className="md:max-w-[350%] flex flex-col gap-y-4 md:gap-y-8">
+              {/* study */}
+              <div>
+                <h4 className="text-xl md:text-2xl font-semibold pl-1.5 mb-3">
+                  Case Study
+                </h4>
+                <h5 className="border border-themeGray rounded-full py-2.5 px-4 text-center leading-none text-sm font-semibold capitalize inline-flex">
+                  {projectData?.study}
+                </h5>
+              </div>
+              {/* platform */}
+              <div>
+                <h4 className="text-xl md:text-2xl font-semibold pl-1.5 mb-3">
+                  CMS Platform
+                </h4>
+                <h5 className="border border-themeGray rounded-full py-2.5 px-4 text-center leading-none text-sm font-semibold capitalize inline-flex">
+                  {projectData?.platform}
+                </h5>
+              </div>
+              {/* vibe */}
+              <div>
+                <h4 className="text-xl md:text-2xl font-semibold pl-1.5 mb-3">
+                  Vibe
+                </h4>
+                <h5 className="border border-themeGray rounded-full py-2.5 px-4 text-center !leading-[1.3] text-sm font-semibold capitalize inline-flex">
+                  {projectData?.vibe}
+                </h5>
+              </div>
+            </div>
+
+            {/* title and desc */}
+            <div className="md:max-w-[35%] flex justify-between flex-col gap-y-4 mb-12 md:mb-0">
+              <h4 className="text-2xl font-medium font-theme md:mb-12">
+                {projectData?.title}
+              </h4>
+              <div>
+                <p>{projectData?.desc}</p>
+                <div className="mt-8">
+                  <SecButton />
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* top images */}
+          <div className="flex flex-col md:flex-row gap-4 py-12 md:py-20">
+            {projectData?.topImages?.length > 0 && (
+              <>
+                <img
+                  src={projectData.topImages[0] || ''}
+                  alt="gallery image"
+                  className="object-cover w-full md:w-[38%]"
+                />
+                <img
+                  src={projectData.topImages[1] || ''}
+                  alt="gallery image"
+                  className="object-cover w-full md:w-[24%]"
+                />
+                <img
+                  src={projectData.topImages[2] || ''}
+                  alt="gallery image"
+                  className="object-cover w-full md:w-[38%]"
+                />
+              </>
+            )}
+          </div>
+
+          {/* main image */}
+          <div className="flex justify-center items-center py-12">
             <img
-              src={`${imgUrl}/${projectData?.bannerImage}`}
-              alt="banner Image"
+              src={projectData?.mainImage || ''}
+              alt="main image"
+              className=""
             />
           </div>
 
-          {/* marquee */}
-          <div className="w-full border-y border-[#577C8E] text-theme py-4">
-            <Marquee speed={50}>
-              {texts.map((item, i) => (
-                <h4 key={i} className="text-lg capitalize">
-                  {item}
-                </h4>
-              ))}
-            </Marquee>
+          {/* bottom images */}
+          <div className="flex flex-col md:flex-row gap-4 pt-12 md:pt-20">
+            {projectData?.bottomImages?.length > 0 && (
+              <>
+                <img
+                  src={projectData.bottomImages[0] || ''}
+                  alt="gallery image"
+                  className="w-full object-cover md:w-1/2"
+                />
+                <div className="w-full md:w-1/2 flex flex-col gap-y-4">
+                  <img
+                    src={projectData.bottomImages[1] || ''}
+                    alt="gallery image"
+                    className="w-full object-cover"
+                  />
+                  <img
+                    src={projectData.bottomImages[2] || ''}
+                    alt="gallery image"
+                    className="w-full object-cover"
+                  />
+                </div>
+              </>
+            )}
           </div>
 
-          <div className="container">
-            {/* content */}
-            <div className="flex flex-col-reverse md:flex-row justify-between md:items-center gap-4 my-12">
-              <div className="md:max-w-[350%] flex flex-col gap-y-4 md:gap-y-8">
-                {/* study */}
-                <div>
-                  <h4 className="text-xl md:text-2xl font-semibold pl-1.5 mb-3">
-                    Case Study
-                  </h4>
-                  <h5 className="border border-themeGray rounded-full py-2.5 px-4 text-center leading-none text-sm font-semibold capitalize inline-flex">
-                    {project.study}
-                  </h5>
-                </div>
-                {/* platform */}
-                <div>
-                  <h4 className="text-xl md:text-2xl font-semibold pl-1.5 mb-3">
-                    CMS Platform
-                  </h4>
-                  <h5 className="border border-themeGray rounded-full py-2.5 px-4 text-center leading-none text-sm font-semibold capitalize inline-flex">
-                    {project.platform}
-                  </h5>
-                </div>
-                {/* vibe */}
-                <div>
-                  <h4 className="text-xl md:text-2xl font-semibold pl-1.5 mb-3">
-                    Vibe
-                  </h4>
-                  <h5 className="border border-themeGray rounded-full py-2.5 px-4 text-center !leading-[1.3] text-sm font-semibold capitalize inline-flex">
-                    {project.vibe}
-                  </h5>
-                </div>
-              </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4 pt-4">
+            {projectData?.bottomImages?.length > 0 && (
+              <>
+                {/* Left large image (1/3 width) */}
+                <img
+                  src={projectData.bottomImages[3] || ''}
+                  alt="gallery image"
+                  className="w-full h-full object-cover"
+                />
 
-              {/* title and desc */}
-              <div className="md:max-w-[35%] flex justify-between flex-col gap-y-4 mb-12 md:mb-0">
-                <h4 className="text-2xl font-medium font-theme md:mb-12">
-                  {project.title}
-                </h4>
-                <div>
-                  <p>{project.desc}</p>
-                  <div className="mt-8">
-                    <SecButton />
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* top images */}
-            <div className="flex flex-col md:flex-row gap-4 py-12 md:py-20">
-              {projectData?.topImages?.length > 0 && (
-                <>
+                {/* Middle stacked images (1/3 width) */}
+                <div className="grid grid-rows-2 gap-y-4">
                   <img
-                    src={`${imgUrl}/${projectData.topImages[0] || ''}`}
-                    alt="gallery image"
-                    className="object-cover w-full md:w-[38%]"
-                  />
-                  <img
-                    src={`${imgUrl}/${projectData.topImages[1] || ''}`}
-                    alt="gallery image"
-                    className="object-cover w-full md:w-[24%]"
-                  />
-                  <img
-                    src={`${imgUrl}/${projectData.topImages[2] || ''}`}
-                    alt="gallery image"
-                    className="object-cover w-full md:w-[38%]"
-                  />
-                </>
-              )}
-            </div>
-
-            {/* main image */}
-            <div className="flex justify-center items-center py-12">
-              <img
-                src={`${imgUrl}/${projectData.mainImage || ''}`}
-                alt="main image"
-                className=""
-              />
-            </div>
-
-            {/* bottom images */}
-            <div className="flex flex-col md:flex-row gap-4 pt-12 md:pt-20">
-              {projectData?.bottomImages?.length > 0 && (
-                <>
-                  <img
-                    src={`${imgUrl}/${projectData.bottomImages[0] || ''}`}
-                    alt="gallery image"
-                    className="w-full object-cover md:w-1/2"
-                  />
-                  <div className="w-full md:w-1/2 flex flex-col gap-y-4">
-                    <img
-                      src={`${imgUrl}/${projectData.bottomImages[1] || ''}`}
-                      alt="gallery image"
-                      className="w-full object-cover"
-                    />
-                    <img
-                      src={`${imgUrl}/${projectData.bottomImages[2] || ''}`}
-                      alt="gallery image"
-                      className="w-full object-cover"
-                    />
-                  </div>
-                </>
-              )}
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 pt-4">
-              {projectData?.bottomImages?.length > 0 && (
-                <>
-                  {/* Left large image (1/3 width) */}
-                  <img
-                    src={`${imgUrl}/${projectData.bottomImages[0] || ''}`}
+                    src={projectData.bottomImages[4] || ''}
                     alt="gallery image"
                     className="w-full h-full object-cover"
                   />
-
-                  {/* Middle stacked images (1/3 width) */}
-                  <div className="grid grid-rows-2 gap-y-4">
-                    <img
-                      src={`${imgUrl}/${projectData.bottomImages[1] || ''}`}
-                      alt="gallery image"
-                      className="w-full h-full object-cover"
-                    />
-                    <img
-                      src={`${imgUrl}/${projectData.bottomImages[2] || ''}`}
-                      alt="gallery image"
-                      className="w-full h-full object-cover"
-                    />
-                  </div>
-
-                  {/* Right large image (1/3 width) */}
                   <img
-                    src={`${imgUrl}/${projectData.bottomImages[3] || ''}`}
+                    src={projectData.bottomImages[5] || ''}
                     alt="gallery image"
                     className="w-full h-full object-cover"
                   />
-                </>
-              )}
-            </div>
+                </div>
+
+                {/* Right large image (1/3 width) */}
+                <img
+                  src={projectData.bottomImages[6] || ''}
+                  alt="gallery image"
+                  className="w-full h-full object-cover"
+                />
+              </>
+            )}
           </div>
-        </>
-      )}
+        </div>
+      </>
     </>
   );
 };
